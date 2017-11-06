3213f9855c1dfc01926bd21b78cada2d0b626d91
Boris Cherny <boris@performancejs.com>
2017-11-06 11:55:38 -0800

    add comment about Lazy

--- src/types.d.ts
+++ src/types.d.ts
@@ -14,4 +14,12 @@ declare module 'react-autocomplete'
  * Globals
  */
 
+/**
+ * `_.chain(..)` gives a value of type `_.LoDashExplicitWrapper`. `chain` is
+ * convenient because it lets us defer computations over collections and reap
+ * the performance benefits of Lodash's loop fusion.
+ *
+ * We alias this type to `Lazy` and make it available globally for ease of
+ * use and superior readability.
+ */
 type Lazy<T> = _.LoDashExplicitWrapper<T>
