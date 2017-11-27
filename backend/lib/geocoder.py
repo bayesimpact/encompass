10abedd92efecd35cb3dd2a94b6dd922942f0454
Brian Lewis <brian@bayesimpact.org>
2017-11-27 11:52:12 -0800

    Make batch geocoding more resilient to various HTTP errors

--- backend/lib/geocoder.py
+++ backend/lib/geocoder.py
@@ -2,9 +2,21 @@
 import os
 
 from geocodio import GeocodioClient
-from geocodio.exceptions import GeocodioAuthError, GeocodioDataError  # noqa: F401
+from geocodio.exceptions import (
+    GeocodioAuthError, GeocodioDataError, GeocodioServerError)  # noqa: F401
 
 import osmnx as ox
+
+from retrying import retry
+
+
+WAIT_FIXED_MILLISECONDS = 2000
+STOP_MAX_ATTEMPT_NUMBER = 4
+
+
+def _retry_on_geocodio_errors(exception):
+    errors = [GeocodioAuthError, GeocodioDataError, GeocodioServerError]
+    return any(isinstance(exception, error) for error in errors)
 
 
 class GeocodioCoder():
@@ -14,32 +26,47 @@ class GeocodioCoder():
         """Initialize Geocodio geocoder class."""
         self.client = GeocodioClient(api_key)
 
+    @staticmethod
+    def _format_result(address, geocoding_result):
+        """Format an address and geocoding result for use throughout the application."""
+        return {
+            'address': address,
+            'latitude': geocoding_result.coords[0],
+            'longitude': geocoding_result.coords[1],
+        }
+
     def geocode(self, address):
         """Geocode a single point with geocodio."""
         result = self.client.geocode(address)
-        return {
-            'address': address,
-            'latitude': result.coords[0],
-            'longitude': result.coords[1],
-        }
+        return GeocodioCoder._format_result(address=address, geocoding_result=result)
 
     def geocode_batch(self, addresses):
         """
-        Geocode a list of addressed with geocodio.
+        Geocode a list of addresses with geocodio.
 
-        Returns a list of tuples with addresses, and coordinates in the format:
-        [(address, (long, lat)), ...]
+        Returns a list of dictionaries with address, latitude, and longitude.
+        The results are returned in the same order as the original list.
         """
         try:
-            results = self.client.geocode(addresses)
-            return [{
-                'address': address,
-                'latitude': results.get(address).coords[0],
-                'longitude': results.get(address).coords[1],
-            } for address in addresses]
+            results = self._safe_geocode(addresses)
+            return [
+                GeocodioCoder._format_result(
+                    address=address,
+                    geocoding_result=results.get(address)
+                )
+                for address in addresses
+                if results.get(address).coords
+            ]
         except Exception as error:
             print(error, 'geocoding - switching to single geocoding.')
             return [self.geocode(address) for address in addresses]
+
+    @retry(
+        retry_on_exception=_retry_on_geocodio_errors,
+        wait_fixed=WAIT_FIXED_MILLISECONDS,
+        stop_max_attempt_number=STOP_MAX_ATTEMPT_NUMBER)
+    def _safe_geocode(self, addresses):
+        return self.client.geocode(addresses)
 
 
 class OxCoder():
