5ce71438f47bde23786ff4b14b55bafd5c22949d
Boris Cherny <boris@performancejs.com>
2017-12-03 10:51:53 -0800

    fix test

--- test/setupFiles.js
+++ test/setupFiles.js
@@ -11,3 +11,6 @@ global.Headers = Map
 // Mock `requestAnimationFrame` (required for React tests)
 global.requestAnimationFrame = callback =>
   setTimeout(callback, 0)
+
+// Mock `URL.createObjectURL` (required for Mapbox tests)
+global.URL.createObjectURL = () => ({})
