3c6c71a4869a40fac0d924fa6fc3c1047a129a07
Brian Lewis <brian@bayesimpact.org>
2018-01-05 11:27:20 -0800

    Add owner change to county data

--- backend/lib/database/postgres/census_tiger.sh
+++ backend/lib/database/postgres/census_tiger.sh
@@ -11,7 +11,7 @@ PSQL=psql
 SHP2PGSQL=shp2pgsql
 
 # Get Postgres password.
-echo -n Postgres password: 
+echo -n Postgres password:
 read -s password
 export PGPASSWORD=$password
 
@@ -41,7 +41,8 @@ ${PSQL} -c "CREATE SCHEMA tiger_staging;"
 for z in tl_*county.zip ; do $UNZIPTOOL -o -d $TMPDIR $z; done
 cd $TMPDIR;
 
-${PSQL} -c "CREATE TABLE tiger_data.county_all(CONSTRAINT pk_tiger_data_county_all PRIMARY KEY (cntyidfp),CONSTRAINT uidx_tiger_data_county_all_gid UNIQUE (gid)  ) INHERITS(tiger.county); " 
+${PSQL} -c "alter table county owner to ${PGUSER}"
+${PSQL} -c "CREATE TABLE tiger_data.county_all(CONSTRAINT pk_tiger_data_county_all PRIMARY KEY (cntyidfp),CONSTRAINT uidx_tiger_data_county_all_gid UNIQUE (gid)  ) INHERITS(tiger.county); "
 ${SHP2PGSQL} -D -c -s 4269 -g the_geom   -W "latin1" tl_2016_us_county.dbf tiger_staging.county | ${PSQL}
 ${PSQL} -c "ALTER TABLE tiger_staging.county RENAME geoid TO cntyidfp;  SELECT loader_load_staged_data(lower('county'), lower('county_all'));"
 	${PSQL} -c "CREATE INDEX tiger_data_county_the_geom_gist ON tiger_data.county_all USING gist(the_geom);"
@@ -49,4 +50,4 @@ ${PSQL} -c "ALTER TABLE tiger_staging.county RENAME geoid TO cntyidfp;  SELECT l
 	${PSQL} -c "CREATE TABLE tiger_data.county_all_lookup ( CONSTRAINT pk_county_all_lookup PRIMARY KEY (st_code, co_code)) INHERITS (tiger.county_lookup);"
 	${PSQL} -c "VACUUM ANALYZE tiger_data.county_all;"
 	${PSQL} -c "INSERT INTO tiger_data.county_all_lookup(st_code, state, co_code, name) SELECT CAST(s.statefp as integer), s.abbrev, CAST(c.countyfp as integer), c.name FROM tiger_data.county_all As c INNER JOIN state_lookup As s ON s.statefp = c.statefp;"
-	${PSQL} -c "VACUUM ANALYZE tiger_data.county_all_lookup;" 
+	${PSQL} -c "VACUUM ANALYZE tiger_data.county_all_lookup;"
