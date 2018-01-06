af178755468ffbd373661d15692d09d495f9d4fc
philip <philip@bayesimpact.org>
2018-01-05 16:00:42 -0800

    first steps to local db container

--- backend/runners/initialize_postgres.py
+++ backend/runners/initialize_postgres.py
@@ -1,4 +1,6 @@
 """Initialize Postgres tables."""
+from sqlalchemy_utils import database_exists, create_database
+
 from backend.lib.database.postgres import connect
 from backend.lib.database.postgres import postgis
 from backend.lib.database.postgres.base import Base
@@ -8,6 +10,11 @@ from backend.lib.database.tables import address, provider, representative_point,
 def initialize_postgres():
     """Initialize Postgres tables."""
     postgres_engine = connect.create_db_engine(echo=True)
+
+    # Create DB if necessary...
+    if not database_exists(postgres_engine.url):
+        create_database(postgres_engine.url)
+
     # Install Postgis
     postgis.install()
     # Create tables
