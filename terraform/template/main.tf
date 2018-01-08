ba9a0c3cb74a59dfa9c87b30d7369b661e4677d1
philip <philip@bayesimpact.org>
2018-01-08 14:47:23 -0800

    add explicit outbound rules to appserver SG

--- terraform/template/main.tf
+++ terraform/template/main.tf
@@ -79,6 +79,14 @@ resource "aws_security_group" "na_app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # permit everything outbound. needs to be done explicitly
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
   tags {
     Name = "na_app_sg"
   }
