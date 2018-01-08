4724cbadc4706b7eafb8dad41304d4306b1a3697
philip <philip@bayesimpact.org>
2018-01-08 12:03:13 -0800

    bring app db under terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,29 @@ resource "aws_instance" "na_app" {
 
   tags { Name = "na-teddy" }
 }
+
+# this is the rds instance representing the default app db
+resource "aws_db_instance" "na_db" {
+  # these two attributes are required
+  identifier                          = "philip-testing"
+  instance_class                      = "db.t2.large"
+
+  allocated_storage                   = 40
+  auto_minor_version_upgrade          = true
+  backup_retention_period             = 7
+  copy_tags_to_snapshot               = false
+  skip_final_snapshot                 = true
+  db_subnet_group_name                = "default"
+  engine                              = "postgres"
+  engine_version                      = "9.6.3"
+  iam_database_authentication_enabled = false
+  license_model                       = "postgresql-license"
+  monitoring_interval                 = 0
+  multi_az                            = false
+  name                                = "network_adequacy"
+  port                                = 5432
+  publicly_accessible                 = true
+  storage_encrypted                   = false
+  storage_type                        = "gp2"
+  username                            = "tds"
+}
