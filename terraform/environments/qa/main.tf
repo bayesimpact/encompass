6a6de8fbe4e3a9c1d08e4db72d5177a62e84b9b7
philip <philip@bayesimpact.org>
2018-01-11 14:22:18 -0800

    add qa env definition

--- terraform/environments/qa/main.tf
+++ terraform/environments/qa/main.tf
@@ -2,9 +2,9 @@ module "stack" {
   source        = "../../template"
 
   # this can be for my test environment
-  env_name = "philip-test"
-  db_id = "philip-test"
-  instance_name_tag = "philip-test"
+  env_name = "tds-qa"
+  db_id = "tds-qa"
+  instance_name_tag = "tds-qa"
 }
 
 # backend definition can't have interpolation, so unfortunately this does need to be
@@ -12,7 +12,7 @@ module "stack" {
 terraform {
   backend "s3" {
     bucket     = "network-adequacy-terraform"
-    key        = "na-philip-test/terraform.tfstate"
+    key        = "tds-qa/terraform.tfstate"
     region     = "us-west-1"
 
     # ddb table to hold tfstate locks
