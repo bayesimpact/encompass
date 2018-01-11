93734697dbcc4cb05d0bd301575b13a1331c44e2
Eric Boucher <eric.p.boucher@gmail.com>
2018-01-11 13:22:31 -0800

    WIP multiple provider popups

--- frontend/src/components/MapTooltip/MapTooltip.tsx
+++ frontend/src/components/MapTooltip/MapTooltip.tsx
@@ -10,19 +10,18 @@ type MapTooltipProps = {
   coordinates: LngLat
 }
 
-let MapTooltip: React.StatelessComponent<MapTooltipProps> = ({ children, coordinates }) =>
-  <Popup
+let MapTooltip: React.StatelessComponent<MapTooltipProps> = ({ children, coordinates }) => {
+  return (
+    <Popup
     anchor='bottom'
     className='MapTooltip'
     coordinates={coordinates.toArray()}
     offset={[0, -20]}
   >
-    <table>
-      <tbody>
-        {children}
-      </tbody>
-    </table>
+  {children}
   </Popup>
+  )
+}
 
 type Props = {
   point: GeoJSONEventData
@@ -33,20 +32,53 @@ type Props = {
  * not only the first one.
  */
 export let ProviderPopup: React.StatelessComponent<Props> =
-  ({ point: { features, lngLat } }) =>
-    <MapTooltip coordinates={lngLat}>
-      <TableRow name='Address' value={features[0].properties.address} />
-      <TableRow name='Specialty' value={features[0].properties.specialty} />
-      <TableRow name='Lat' value={formatCoordinate(lngLat.lat)} />
-      <TableRow name='Lng' value={formatCoordinate(lngLat.lng)} />
+  ({ point: { features, lngLat } }) => {
+    console.log('features')
+    let index = 0
+    let maxIndex = features.length - 1
+    console.log(maxIndex)
+    return (
+      <MapTooltip coordinates={lngLat}>
+      <table>
+        <tbody>
+          <TableRow name='Address' value={features[0].properties.address} />
+          <TableRow name='Specialty' value={features[0].properties.specialty} />
+          <TableRow name='Lat' value={formatCoordinate(lngLat.lat)} />
+          <TableRow name='Lng' value={formatCoordinate(lngLat.lng)} />
+        </tbody>
+    </table>
+    <div className='controls'>
+      <button className='toggle toggle--prev' onClick={() => _TogglePrev(index)}>Prev</button>
+      <button className='toggle toggle--next' onClick={() => _ToggleNext(index, maxIndex)}> Next</button>
+    </div>
     </MapTooltip>
+    )
+  }
+
+function _ToggleNext(index: number, maxIndex: number) {
+  console.log('toggle next')
+  if (index === maxIndex)
+    return index
+  return index + 1
+}
+
+function _TogglePrev(index: number) {
+  console.log('toggle prev')
+  if (index === 0)
+    return index
+  return index - 1
+}
 
 export let RepresentativePointPopup: React.StatelessComponent<Props> =
   ({ point: { features, lngLat } }) =>
     <MapTooltip coordinates={lngLat}>
-      <TableRow name='County' value={features[0].properties.county} />
-      <TableRow name='ZIP' value={features[0].properties.zip} />
-      <TableRow name='Population' value={features[0].properties.population} />
-      <TableRow name='Lat' value={formatCoordinate(lngLat.lat)} />
-      <TableRow name='Lng' value={formatCoordinate(lngLat.lng)} />
+    <table>
+      <tbody>
+        <TableRow name='County' value={features[0].properties.county} />
+        <TableRow name='ZIP' value={features[0].properties.zip} />
+        <TableRow name='Population' value={features[0].properties.population} />
+        <TableRow name='Lat' value={formatCoordinate(lngLat.lat)} />
+        <TableRow name='Lng' value={formatCoordinate(lngLat.lng)} />
+      </tbody>
+    </table>
     </MapTooltip>
