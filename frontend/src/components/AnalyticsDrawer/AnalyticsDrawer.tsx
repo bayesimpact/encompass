f464d6e4554fa3546b10c93069213a989be2570d
Boris Cherny <boris@performancejs.com>
2018-01-11 21:19:54 -0800

    rm  IconBar, Providers drawer, service areas drawer, Link, InfoBox

--- frontend/src/components/AnalyticsDrawer/AnalyticsDrawer.tsx
+++ frontend/src/components/AnalyticsDrawer/AnalyticsDrawer.tsx
@@ -1,42 +1,12 @@
-import ProvidersIcon from 'mui-icons/cmdi/account-multiple'
-import MarkerIcon from 'mui-icons/cmdi/map-marker'
 import * as React from 'react'
 import { withStore } from '../../services/store'
 import { DownloadAnalysisLink } from '../DownloadAnalysisLink/DownloadAnalysisLink'
-import { InfoBox } from '../InfoBox/InfoBox'
-import { Link } from '../Link/Link'
 import { ServiceAreaSelector } from '../ServiceAreaSelector/ServiceAreaSelector'
 import './AnalyticsDrawer.css'
 import { ServiceAreaAnalytics } from './ServiceAreaAnalytics'
 
-let Analytics = withStore(
-  'providers',
-  'selectedServiceArea',
-  'serviceAreas'
-)(({ store }) => {
-
-  if (store.get('serviceAreas').length === 0) {
-    return <InfoBox large>
-      Please choose a service area in the <Link className='' to='/service-areas'><MarkerIcon /> Service Areas drawer</Link>
-    </InfoBox>
-  }
-
-  if (store.get('providers').length === 0) {
-    return <InfoBox large>
-      Please upload providers in the <Link className='' to='/providers'><ProvidersIcon /> Providers drawer</Link>
-    </InfoBox>
-  }
-
-  return <div>
-    <ServiceAreaAnalytics />
-    <DownloadAnalysisLink />
-  </div>
-
-})
-
 /**
- * TODO: Show loading indicator while CSV is uploading + parsing
- * or necessary data is being fetched.
+ * TODO: Show loading indicator while necessary data is being fetched.
  */
 export let AnalyticsDrawer = withStore('selectedServiceArea')(({ store }) =>
   <div>
@@ -45,6 +15,7 @@ export let AnalyticsDrawer = withStore('selectedServiceArea')(({ store }) =>
       onChange={store.set('selectedServiceArea')}
       value={store.get('selectedServiceArea')}
     />
-    <Analytics />
+    <ServiceAreaAnalytics />
+    <DownloadAnalysisLink />
   </div>
 )
