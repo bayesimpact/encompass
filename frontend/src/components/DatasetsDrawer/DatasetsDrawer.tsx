ae8483b3e44030f57a10b05d1996b360dda72042
Boris Cherny <boris@performancejs.com>
2018-01-13 15:52:43 -0800

    create tile component, hook it up, add mock datsets (fix #198, fix #199, fix #200) (#246)

---
+++ frontend/src/components/DatasetsDrawer/DatasetsDrawer.tsx
@@ -0,0 +1,41 @@
+import { memoize } from 'lodash'
+import * as React from 'react'
+import { DATASETS } from '../../constants/datasets'
+import { Dataset } from '../../constants/datatypes'
+import { withStore } from '../../services/store'
+import { Tile, TilePicker } from '../TilePicker/TilePicker'
+import './DatasetsDrawer.css'
+
+const COLORS = [
+  '#5f7c8a',
+  '#47488e',
+  '#478e8c',
+  '#478e51',
+  '#8e7947',
+  '#8e4747',
+  '#8e4770'
+]
+
+let toTiles = memoize((datasets: Dataset[]): Tile<Dataset>[] =>
+  datasets.map((_, n) => ({
+    color: COLORS[n],
+    data: _,
+    description: _.description,
+    name: _.name
+  }))
+)
+
+let tiles = toTiles(DATASETS)
+let DatasetTilePicker = TilePicker<Dataset>()
+
+export let DatasetsDrawer = withStore('selectedDataset')(({ store }) =>
+  <div className='DatasetsDrawer'>
+    <h2>Choose a dataset to explore</h2>
+    <DatasetTilePicker
+      onChange={tile => store.set('selectedDataset')(tile.data)}
+      tiles={tiles}
+      value={tiles.find(_ => _.data === store.get('selectedDataset')) || null}
+    />
+    <p className='Center LargeFont Muted'>Don't see the dataset you want? <a href='mailto:data@bayesimpact.org?subject=Request a dataset'>Email us</a>.</p>
+  </div>
+)
--- frontend/src/components/LeftPane/LeftPane.tsx
+++ frontend/src/components/LeftPane/LeftPane.tsx
@@ -3,16 +3,18 @@ import Drawer from 'material-ui/Drawer'
 import * as React from 'react'
 import { withStore } from '../../services/store'
 import { AnalyticsDrawer } from '../AnalyticsDrawer/AnalyticsDrawer'
+import { DatasetsDrawer } from '../DatasetsDrawer/DatasetsDrawer'
 import './LeftPane.css'
 
 let drawers = {
-  '/analytics': AnalyticsDrawer
+  '/analytics': AnalyticsDrawer,
+  '/datasets': DatasetsDrawer
 }
 
 export let LeftPane = withStore('route')(({ store }) =>
   <div className={'LeftPane' + (store.get('route') === '/' ? '' : ' -isOpen')}>
     {map(drawers, (Component, route) =>
-      <Drawer className='LeftPaneContent' key={route} open={route === store.get('route')} width={320}>
+      <Drawer className='LeftPaneContent' key={route} open={route === store.get('route')} width='40%'>
         <Component />
       </Drawer>
     )}
