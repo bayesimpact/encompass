63d4536a3330d76d56fde6a57e722a8caecf16d4
Eric Boucher <eric.p.boucher@gmail.com>
2018-01-18 17:11:42 -0800

    Fix missing service areas

--- frontend/src/components/Uploader/ServiceAreasUplodader.tsx
+++ frontend/src/components/Uploader/ServiceAreasUplodader.tsx
@@ -6,7 +6,6 @@ import { ZIPS_BY_COUNTY_BY_STATE } from '../../constants/zipCodesByCountyByState
 import { Store, withStore } from '../../services/store'
 import { ColumnDefinition, isEmpty, ParseError, parseRows } from '../../utils/csv'
 import { serializeServiceArea } from '../../utils/serializers'
-import { capitalizeWords } from '../../utils/string'
 import { ClearInputsButton } from '../ClearInputsButton/ClearInputsButton'
 import { CSVUploader } from '../CSVUploader/CSVUploader'
 import { StateSelector } from '../StateSelector/StateSelector'
@@ -75,7 +74,7 @@ const COLUMNS = [
 
 let parse = parseRows<[string, string][], { state: State }>(COLUMNS, (([county, zip], rowIndex, { state }) => {
 
-  // validate that zip exists
+  // Validate that zip exists
   if (zip && !(zip in COUNTIES_BY_ZIP)) {
     return new ParseError(rowIndex, 1, COLUMNS[1], `We don't support the zip code "${zip}" yet. Reach out to us at health@bayesimpact.org`)
   }
@@ -83,15 +82,16 @@ let parse = parseRows<[string, string][], { state: State }>(COLUMNS, (([county, 
   // We infer missing zips and counties, so one zip might map to
   // more than one county, and one county maps to many zips.
   let pairs = getCountyAndZip(state, county, zip)
-    .map(([county, zip]) => [capitalizeWords(county), zip] as [string, string])
+    .map(([county, zip]) => [county, zip] as [string, string])
 
-  // validate that counties exist
-  let badCounty = pairs.value().find(([county]) => !(county in ZIPS_BY_COUNTY_BY_STATE[state]))
+  // Validate that counties exist
+  let badCounty = pairs.value().find(([county]) => Object.keys(ZIPS_BY_COUNTY_BY_STATE[state]).indexOf(county!) <= -1)
+
   if (badCounty) {
     return new ParseError(rowIndex, 0, COLUMNS[0], `We don't support county "${badCounty[0]}" yet. Reach out to us at health@bayesimpact.org.`)
   }
 
-  // validate that zip code is in county
+  // Validate that zip code is in county
   // TODO: consider pre-hashing zips for O(1) lookup
   let badZip = pairs.value().find(([county, zip]) => !ZIPS_BY_COUNTY_BY_STATE[state][county].includes(zip))
   if (badZip) {
@@ -121,7 +121,7 @@ function getCountyAndZip(state: State, county: string | null, zip: string | null
     case ParseMode.INFER_ZIP:
       // If county is defined but zip isn't, default to all zips for
       // the given county.
-      return chain(ZIPS_BY_COUNTY_BY_STATE[state][capitalizeWords(county!)])
+      return chain(ZIPS_BY_COUNTY_BY_STATE[state][county!])
         .map(zip => [county, zip] as [string, string])
 
     case ParseMode.WELL_FORMED:
