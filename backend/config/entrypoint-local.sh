48c1a813744e200a8c06c5a9009f29decd54ccd1
philip <philip@bayesimpact.org>
2018-02-01 11:44:26 -0800

    fix variable assignment in local entrypoint

--- backend/config/entrypoint-local.sh
+++ backend/config/entrypoint-local.sh
@@ -2,7 +2,7 @@
 echo "Waiting for DB to become available..."
 GRACE_PERIOD=5
 if [[ -v DB_GRACE_PERIOD ]]; then
-    GRACE_PERIOD = ${DB_GRACE_PERIOD}
+    GRACE_PERIOD=${DB_GRACE_PERIOD}
 fi
 sleep ${GRACE_PERIOD} # FIXME Wait for DB server to become available
 python runners/initialize_postgres.py
