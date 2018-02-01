af99b63d9cbffeeff7b79f2c716856d72a1fd67a
Brian Lewis <brian@bayesimpact.org>
2018-02-01 13:20:41 -0800

    Revamp census tract load script to use full ACS tables

--- backend/runners/load_census_tract_demographics.py
+++ backend/runners/load_census_tract_demographics.py
@@ -1,75 +1,128 @@
-"""Methods to upload demographic data to the Postgres database."""
-import multiprocessing.dummy
-import re
+"""
+Methods to upload demographic data to the Postgres database.
+
+The csv files come from the ACS 5-year estimates and can be found using American Fact Finder:
+e.g.,
+    https://factfinder.census.gov/faces/tableservices/jsf/pages/
+    productview.xhtml?pid=ACS_16_5YR_DP02&prodType=table
+"""
+import csv
 
 from backend.lib.database.postgres import connect
 
 import pandas as pd
 
 N_THREADS = 100
-CENSUS_TABLE_NAME = 'census_tract_demographics'
+
+UNUSED_COLUMNS = ['GEO.display-label', 'GEO.id']
 
 
-def _drop_census_table_if_exists():
-    """Drop the census demographics table if it already exists."""
+def _drop_table_if_exists(table_name):
+    """Drop the provided table if it already exists."""
+    print('Dropping {} (if such a table exists).'.format(table_name))
     engine = connect.create_db_engine()
-    engine.execute('DROP TABLE IF EXISTS {};'.format(CENSUS_TABLE_NAME))
+    engine.execute('DROP TABLE IF EXISTS {};'.format(table_name))
 
 
-def _read_census_data_in_batches(filepath='data/census/acs2015_census_tract_data.csv'):
-    dataframes = list(
-        pd.read_csv(
-            filepath,
-            dtype={'CensusTract': str},
-            chunksize=500,
-        )
-    )
-    return [_clean_dataframe(df) for df in dataframes]
+def _clean_column_name(column_name):
+    """Transform a column name into a format accepted by Postgres."""
+    return column_name.lower().replace('.', '_').replace('-', '_')
 
 
-def _convert_string_to_snake_case(input):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', input)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
-
-
-def _clean_dataframe(df):
-    """Make columns snake_case and zero-pad census tract information."""
+def _clean_csv(raw_csv_path, output_path):
+    """Clean a csv, rewriting to the specified filepath."""
+    print('Cleaning csv {}.'.format(raw_csv_path))
+    df = pd.read_csv(raw_csv_path, skiprows=[1], encoding='latin-1')
+    df.drop(columns=UNUSED_COLUMNS, inplace=True)
     df.columns = [
-        _convert_string_to_snake_case(col)
+        _clean_column_name(col)
         for col in df.columns
     ]
-    df['census_tract'] = df['census_tract'].apply(
-        lambda tract_string:
-            '0' + tract_string if len(tract_string) == 10
-            else tract_string
-    )
-    return df
-
-
-def _upload_single_dataframe(df):
-    engine = connect.create_db_engine()
-    df.to_sql(
-        name=CENSUS_TABLE_NAME,
-        con=engine,
-        if_exists='append',
+    df = df.apply(pd.to_numeric, args=('coerce',))
+    df.to_csv(
+        path_or_buf=output_path,
+        header=False,
         index=False,
-        index_label='census_tract'
     )
+    return output_path
 
 
-def upload_all_dataframes(dataframes):
-    """
-    Upload all dataframes to the specified table using multithreading.
+def _create_census_table_from_csv(raw_csv_path, target_table):
+    """Create a census table using the columns from the provided csv."""
+    print('Creating table {}.'.format(target_table))
+    with open(raw_csv_path, 'r', newline='') as csv_file:
+        reader = csv.reader(csv_file)
+        columns = next(reader)
 
-    Re-creates the table if it does not yet exist.
-    """
-    # Ensure that the table exists by running the first INSERT statement separately.
-    _upload_single_dataframe(dataframes[0])
-    with multiprocessing.dummy.Pool(N_THREADS) as executor:
-        executor.map(_upload_single_dataframe, dataframes[1:])
+    columns = [
+        _clean_column_name(col) for col in columns if col not in UNUSED_COLUMNS
+    ]
+    datatypes = [
+        'VARCHAR' if col == 'geo_id2' else 'DECIMAL'
+        for col in columns
+    ]
+    columns_with_types = [
+        '{} {}'.format(col, dtype)
+        for col, dtype in zip(columns, datatypes)
+    ]
+    if 'geo_id2' in columns:
+        primary_key_index = columns.index('geo_id2')
+        columns_with_types[primary_key_index] += ' PRIMARY KEY '
+
+    create_table_query = """
+        CREATE TABLE {target_table} (
+            {columns_with_types}
+        )
+        ;
+    """.format(
+        target_table=target_table,
+        columns_with_types=', '.join(columns_with_types)
+    )
+    engine = connect.create_db_engine()
+    engine.execute(create_table_query)
+
+
+def _upload_csv(csv_path, target_table, sep=','):
+    """Upload a (headerless) csv to the target table via a COPY command."""
+    print('Uploading {} to table {}.'.format(csv_path, target_table))
+    engine = connect.create_db_engine()
+    conn = engine.raw_connection()
+    cur = conn.cursor()
+    with open(csv_path, 'r') as csv_file:
+        cur.copy_expert(
+            sql="""
+                COPY {} FROM STDIN
+                WITH CSV
+                DELIMITER AS ','
+            """.format(target_table),
+            file=csv_file
+        )
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
 if __name__ == '__main__':
-    _drop_census_table_if_exists()
-    dataframes = _read_census_data_in_batches()
-    upload_all_dataframes(dataframes)
+    table_names = [
+        'census_acs_dp_02',
+        'census_acs_dp_03',
+        'census_acs_dp_04',
+        'census_acs_dp_05'
+    ]
+    raw_filepaths = [
+        'data/census/data-profile-tables/raw/ACS_16_5YR_DP02_with_ann.csv',
+        'data/census/data-profile-tables/raw/ACS_16_5YR_DP03_with_ann.csv',
+        'data/census/data-profile-tables/raw/ACS_16_5YR_DP04_with_ann.csv',
+        'data/census/data-profile-tables/raw/ACS_16_5YR_DP05_with_ann.csv',
+    ]
+    clean_filepaths = [
+        'data/census/data-profile-tables/acs_16_5yr_dp02.csv',
+        'data/census/data-profile-tables/acs_16_5yr_dp03.csv',
+        'data/census/data-profile-tables/acs_16_5yr_dp04.csv',
+        'data/census/data-profile-tables/acs_16_5yr_dp05.csv',
+    ]
+    for target_table, raw_path, clean_path in zip(table_names, raw_filepaths, clean_filepaths):
+        _drop_table_if_exists(target_table)
+        _create_census_table_from_csv(raw_path, target_table)
+        _clean_csv(raw_path, clean_path)
+        _upload_csv(clean_path, target_table)
