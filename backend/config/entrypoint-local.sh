d91935b9cfaecb6fb032424444b2a64a726a8331
philip <philip@bayesimpact.org>
2018-02-01 11:42:04 -0800

    add parameter for db grace period for ci

--- backend/config/entrypoint-local.sh
+++ backend/config/entrypoint-local.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "Waiting for DB to become available..."
-sleep 20s # FIXME Wait for DB server to become available
+GRACE_PERIOD=5
+if [[ -v DB_GRACE_PERIOD ]]; then
+    GRACE_PERIOD = ${DB_GRACE_PERIOD}
+fi
+sleep ${GRACE_PERIOD} # FIXME Wait for DB server to become available
 python runners/initialize_postgres.py
 python runners/check_sample_points_exist.py
 FIRST_INIT=$?
