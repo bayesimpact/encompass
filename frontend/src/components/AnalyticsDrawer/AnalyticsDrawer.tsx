446909dc10595aab8a788ea4345ae04998b9881f
philip <philip@bayesimpact.org>
2018-02-12 16:50:23 -0800

    Add census info and charts

--- frontend/src/components/AnalyticsDrawer/AnalyticsDrawer.tsx
+++ frontend/src/components/AnalyticsDrawer/AnalyticsDrawer.tsx
@@ -2,14 +2,16 @@ import * as React from 'react'
 import { withStore } from '../../services/store'
 import { DownloadAnalysisLink } from '../DownloadAnalysisLink/DownloadAnalysisLink'
 import { BackLink } from '../Link/Link'
-import { ServiceAreaSelector } from '../ServiceAreaSelector/ServiceAreaSelector'
+import { CensusCategorySelector } from '../Selectors/CensusCategorySelector'
+import { FormatSelector } from '../Selectors/FormatSelector'
+import { ServiceAreaSelector } from '../Selectors/ServiceAreaSelector'
 import './AnalyticsDrawer.css'
-import { ServiceAreaAnalytics } from './ServiceAreaAnalytics'
+import { CensusAnalytics } from './CensusAnalytics'
 
 /**
  * TODO: Show loading indicator while necessary data is being fetched.
  */
-export let AnalyticsDrawer = withStore('selectedDataset', 'selectedCounties')(({ store }) => {
+export let AnalyticsDrawer = withStore('selectedDataset', 'selectedCensusCategory', 'selectedCounties', 'selectedFormat')(({ store }) => {
 
   let selectedDataset = store.get('selectedDataset')
 
@@ -27,15 +29,29 @@ export let AnalyticsDrawer = withStore('selectedDataset', 'selectedCounties')(({
       <strong className='MediumWeight Muted'>Data sources:</strong>
       <div>{selectedDataset.dataSources.map(_ => <p key={_}>{_}</p>)}</div>
     </div>
-    <div className='ServiceAreas'>
-      <strong className='MediumWeight Muted'>Service areas:</strong>
+    <div className='Selectors'>
+      <strong className='MediumWeight Muted'>Service areas</strong>
       <ServiceAreaSelector
         onChange={store.set('selectedCounties')}
         value={store.get('selectedCounties')}
       />
+      <div>
+        <strong className='MediumWeight Muted'>Demographic</strong>
+        <CensusCategorySelector
+          onChange={store.set('selectedCensusCategory')}
+          value={store.get('selectedCensusCategory')}
+        />
+      </div>
+      <div>
+        <strong className='MediumWeight Muted'>Format</strong>
+        <FormatSelector
+          onChange={store.set('selectedFormat')}
+          value={store.get('selectedFormat')}
+        />
+      </div>
     </div>
-    <div className='Analytics'>
-      <ServiceAreaAnalytics />
+    <div className='CensusAnalytics'>
+      <CensusAnalytics />
     </div>
     <div className='DownloadLink'>
       <DownloadAnalysisLink />
--- frontend/src/components/StatsBox/StatsBox.tsx
+++ frontend/src/components/StatsBox/StatsBox.tsx
@@ -3,17 +3,19 @@ import './StatsBox.css'
 
 type Props = {
   className?: string
+  withFixedColumns?: true
   withBorders?: true
   withHorizontalLines?: true
 }
 
 export let StatsBox: React.StatelessComponent<Props> = ({
-  children, className, withBorders, withHorizontalLines
+  children, className, withFixedColumns, withBorders, withHorizontalLines
 }) =>
   <table className={
     'StatsBox'
     + (className ? ` ${className}` : '')
     + (withBorders ? ' -withBorders' : '')
+    + (withFixedColumns ? ' -withFixedColumnns' : '')
     + (withHorizontalLines ? ' -withHorizontalLines' : '')
   }>
     <tbody>{children}</tbody>
