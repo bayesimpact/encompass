3e5a68b7cf37e55d36c955dce9c8832b2656fe05
Eric Boucher <eric.p.boucher@gmail.com>
2018-02-13 23:54:02 -0800

    Simplify code

--- frontend/src/components/Selectors/FormatSelector.tsx
+++ frontend/src/components/Selectors/FormatSelector.tsx
@@ -1,28 +1,24 @@
+import { chain } from 'lodash'
 import { DropDownMenu } from 'material-ui/DropDownMenu'
 import MenuItem from 'material-ui/MenuItem'
 import * as React from 'react'
 import { Format } from '../../constants/datatypes'
-import { withStore } from '../../services/store'
 
 type Props = {
   onChange(format: Format): void
   value: Format
 }
 
-let menuItems: any = []
 let options: Format[] = ['Percentage', 'Number']
-options.forEach(format => {
-  menuItems.push(<MenuItem value={format} key={format} primaryText={format} />)
-})
+let menuItems = chain(options).map(
+  _ => <MenuItem value={_} key={_} primaryText={_} />
+).value()
 
-export let FormatSelector = withStore('selectedFormat')<Props>(({ onChange, value }) => {
-  return (
-    <DropDownMenu
-      onChange={(_event, _index, value) => onChange(value)}
-      value={value}>
-      {menuItems}
-    </DropDownMenu >
-  )
-})
+export let FormatSelector: React.StatelessComponent<Props> = ({ onChange, value }) =>
+  <DropDownMenu
+    onChange={(_event, _index, value) => onChange(value)}
+    value={value}>
+    {menuItems}
+  </DropDownMenu >
 
 FormatSelector.displayName = 'FormatSelector'
