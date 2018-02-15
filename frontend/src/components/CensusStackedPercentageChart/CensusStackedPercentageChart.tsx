c4c782e8e2af21b033b9cec590dc821a86a660ef
Eric Boucher <eric.p.boucher@gmail.com>
2018-02-14 17:51:37 -0800

    Add stacked percentage census chart (#394)

---
+++ frontend/src/components/CensusStackedPercentageChart/CensusStackedPercentageChart.tsx
@@ -0,0 +1,46 @@
+import 'chart.piecelabel.js'
+import 'chartjs-plugin-stacked100'
+import * as React from 'react'
+import { HorizontalBar } from 'react-chartjs-2'
+import { ADEQUACY_COLORS } from '../../constants/colors'
+import { AdequacyMode, Method } from '../../constants/datatypes'
+import { StatisticsByGroup } from '../../utils/data'
+import { getLegend } from '../MapLegend/MapLegend'
+
+type Props = {
+    measurementMethod: Method,
+    censusGroups: string[],
+    populationByAdequacyByGroup: StatisticsByGroup
+  }
+
+export let CensusStackedPercentageChart: React.StatelessComponent<Props> = ({ measurementMethod, censusGroups, populationByAdequacyByGroup}) => {
+    let adequacyModes = [AdequacyMode.ADEQUATE_15, AdequacyMode.ADEQUATE_30, AdequacyMode.ADEQUATE_60, AdequacyMode.INADEQUATE]
+    let datasets = adequacyModes.map((mode, idx) => {
+        return {
+        backgroundColor: ADEQUACY_COLORS[mode],
+        label: getLegend(measurementMethod, mode),
+        data: censusGroups.map(_ => populationByAdequacyByGroup[_][idx])
+    }})
+
+    return (
+        <HorizontalBar
+            data={{
+                labels: censusGroups,
+                datasets
+            }}
+            options={{
+                scales: {
+                    yAxes: [
+                        { stacked: true }
+                    ],
+                    xAxes: [{
+                        scaleLabel: { display: true, labelString: 'Percentage of Population by Access' }
+                    }]
+                },
+                plugins: {
+                    stacked100: { enable: true }
+                }
+            }}
+        />
+    )
+}
