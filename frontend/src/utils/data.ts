f01fedb2af03a0cc41102c81524fd87f42f91bba
Eric Boucher <eric.p.boucher@gmail.com>
2018-02-14 16:45:35 -0800

    Use 'Total'

--- frontend/src/utils/data.ts
+++ frontend/src/utils/data.ts
@@ -64,7 +64,7 @@ export function summaryStatisticsByServiceAreaAndCensus(
   censusCategoryGroups.forEach(censusGroup => {
     statisticsByGroup[censusGroup] = summaryStatistics(rps, adequacies, { censusCategory, censusGroup })
   })
-  statisticsByGroup[' Total Population'] = summaryStatistics(rps, adequacies)
+  statisticsByGroup[' Total'] = summaryStatistics(rps, adequacies)
   return statisticsByGroup
 }
 
