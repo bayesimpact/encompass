cc61290a8d724f51b63faa775d797035ae6100d3
Eric Boucher <eric.p.boucher@gmail.com>
2018-02-20 14:57:01 -0800

    Add summary table

--- frontend/src/components/AnalyticsDrawer/CensusAnalytics.tsx
+++ frontend/src/components/AnalyticsDrawer/CensusAnalytics.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
 import { withStore } from '../../services/store'
+import { summaryStatisticsByServiceArea } from '../../utils/data'
+import { formatNumber } from '../../utils/formatters'
 import { AdequacyCensusCharts } from '../AdequacyCensusCharts/AdequacyCensusCharts'
+import { StatsBox } from '../StatsBox/StatsBox'
 import './CensusAnalytics.css'
 
 export let CensusAnalytics = withStore(
@@ -11,8 +14,23 @@ export let CensusAnalytics = withStore(
 
   let selectedServiceAreas = store.get('selectedServiceAreas') ? store.get('selectedServiceAreas')! : store.get('serviceAreas')
   let selectedCensusCategory = store.get('selectedCensusCategory')
+  let populationByAdequacy = summaryStatisticsByServiceArea(selectedServiceAreas, store)
+  let totalPopulation = populationByAdequacy.reduce(function (a, b) { return a + b }, 0)
+  let totalProviders = store.get('providers').length
 
   return <div className='CensusAnalytics'>
+    <StatsBox className='HighLevelStats -withFixedColumnns' withBorders>
+      <tr>
+        <th>Total Population</th>
+        <th>Providers</th>
+        <th>Ratio</th>
+      </tr>
+      <tr>
+        <td>{formatNumber(totalPopulation)}</td>
+        <td>{formatNumber(totalProviders)}</td>
+        <td>{formatNumber(totalProviders / totalPopulation)}</td>
+      </tr>
+    </StatsBox>
     <AdequacyCensusCharts serviceAreas={selectedServiceAreas} censusCategory={selectedCensusCategory} />
   </div>
 })
--- frontend/src/utils/formatters.ts
+++ frontend/src/utils/formatters.ts
@@ -31,7 +31,7 @@ export function formatNumber(n: number) {
       if (Number.isNaN(n)) {
         return '-'
       }
-      return n.toLocaleString()
+      return n.toFixed(0).toLocaleString()
   }
 }
 
