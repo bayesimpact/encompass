6b31dc46cc8cfef251dc7037af794e40d75cada4
Brian Lewis <brian@bayesimpact.org>
2018-02-20 11:26:27 -0800

    Update scripts to extract plan info from Healthcare.gov

--- explorer/scripts/healthcare_gov_url_to_csv.py
+++ explorer/scripts/healthcare_gov_url_to_csv.py
@@ -1,13 +1,13 @@
 #!/usr/local/bin/python3.6
 """
-Create csvs from healthcare.gov data extracted from files or from the website directly.
+Create CSVs from healthcare.gov data extracted from files or from the website directly.
 
-The data is filtered by plan_ids for a given state, and by provider address to include
-only in-state or neighboring state (if provided) addresses.
+The data is filtered by plan_ids for a given state and by provider address to include
+only in-state (or neighboring state) addresses.
 
-Csvs are output into data/healthcare_gov/[issuer_id].csv
+The CSVs are output into data/healthcare_gov/[issuer_id].csv
 
-This script will also output the number of providers per plan.
+This script will also echo plan statistics (e.g., the number of providers per plan).
 """
 import argparse
 import ijson
@@ -24,7 +24,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 def normalize_provider(provider, plan_ids_set):
-    """Flatten provider json blobs and filter by plan id."""
+    """Flatten provider JSON blobs and filter by plan ID."""
     normalized = []
     addresses = provider.pop('addresses')
     plans = provider.pop('plans')
@@ -47,29 +47,42 @@ def filter_by_states(providers, states):
 
 
 def _main(**kwargs):
-    """Manually kickoff the json to csv file transformation for a given state."""
+    """Manually kickoff the JSON to CSV file transformation for a given state."""
     state = kwargs['state']
-    # Incorporate neighboring states if present
+    # Incorporate neighboring states if present.
     states = [state]
     if kwargs['neighboring_states']:
         states.extend(kwargs['neighboring_states'])
+    states = set(states)
 
-    logging.info('Starting up translation process for {}'.format(state))
+    logging.info('Starting translation process for {}.'.format(state))
 
     plans = etl_helper.extract_plans(state)
-    logging.info('There are {} plans listed in {}'.format(len(plans), state))
-    logging.info('{}'.format([plan[0] for plan in plans]))
+    logging.info('There are {} plans listed in {}.'.format(len(plans), state))
+    logging.info('The plan IDs are: {}'.format([plan[0] for plan in plans]))
 
-    # TODO: Use Plan Attributes file to exclude all dental plans.
+    # TODO: Use Plan Attributes file to automatically exclude all dental plans.
     # Exclude dentists if the user wants.
     dental_plan_urls = [plan[1] for plan in plans if 'dent' in plan[1].lower()]
     if dental_plan_urls and etl_helper.query_yes_no(
-            message="Would you like to exclude these dental plans? {}".format(dental_plan_urls)):
+            message='Would you like to exclude these {} dental plans? {}'.format(
+                len(dental_plan_urls), dental_plan_urls)):
         plans = [plan for plan in plans if not plan[1] in dental_plan_urls]
 
+    humana_plan_urls = [plan[1] for plan in plans if 'humana' in plan[1].lower()]
+    if humana_plan_urls and etl_helper.query_yes_no(
+            message='Would you like to exclude these {} Humana plans? {}'.format(
+                len(humana_plan_urls), humana_plan_urls)):
+        plans = [plan for plan in plans if not plan[1] in humana_plan_urls]
+
+    logging.info('{} plans in {} remain.'.format(len(plans), state))
+    logging.info('The plan IDs are: {} .'.format([plan[0] for plan in plans]))
+
     count_by_plan = {}
-    for issuer_id, plan_url in plans:
-        logging.info('Processing plan {} at url {}'.format(issuer_id, plan_url))
+    # TODO: Write an extract_plan function.
+    for idx, (issuer_id, plan_url) in enumerate(plans):
+        logging.info('Processing plan {} at url {} .'.format(issuer_id, plan_url))
+        logging.info('This is plan number {} of {}.'.format(idx + 1, len(plans)))
         plan_ids_set = etl_helper.get_issuer_plan_ids(issuer_id)
 
         output_path = etl_helper.HEALTHCARE_GOV_PATH + '/{}/{}.csv'.format(
@@ -85,89 +98,91 @@ def _main(**kwargs):
                 'Error fetching provider urls for {}. Moving on...'.format(issuer_id, plan_url))
             continue
 
-        logging.info('There are {} provider urls for this plan'.format(len(provider_urls)))
+        logging.info('There are {} provider urls for this plan.'.format(len(provider_urls)))
 
         # Exclude pharmacists and facilities if the user wants.
         pharma_urls = [url for url in provider_urls if 'pharma' in url.lower()]
         if pharma_urls and etl_helper.query_yes_no(
-                message="Would you like to exclude these pharma urls? {}".format(pharma_urls)):
+                message='Would you like to exclude these pharma urls? {}'.format(pharma_urls)):
             provider_urls = [url for url in provider_urls if url not in pharma_urls]
 
         providers = []
-        for url in provider_urls:
+        for url_idx, url in enumerate(provider_urls):
+            logging.info('Processing {}.'.format(url))
+            logging.info('Processing url number {} of {}.'.format(url_idx + 1, len(provider_urls)))
             try:
                 if kwargs['from_file']:
                     target_path = etl_helper.HEALTHCARE_GOV_PATH + '/{}/{}/{}.json'.format(
                         state, etl_helper.clean_plan_name(issuer_id), etl_helper.clean_paths(url)
                     )
-
                     if not os.path.exists(target_path):
-                        logging.warning('Filepath {} doesn\'t exist. Skipping.'.format(target_path))
+                        logging.warning("Filepath {} doesn't exist. Skipping.".format(target_path))
                         continue
-                    file = open(target_path, 'r')
+                    _file = open(target_path, 'r', encoding='latin-1')
                 else:
-                    file = urllib.request.urlopen(url)
+                    _file = urllib.request.urlopen(url)
 
-                objects = ijson.items(file, 'item')
+                objects = ijson.items(_file, 'item')
                 for provider in objects:
                     if provider['type'] != 'INDIVIDUAL':
                         continue
                     normalized_providers = normalize_provider(provider, plan_ids_set)
-
                     filtered_by_state = filter_by_states(normalized_providers, states)
                     providers.extend(filtered_by_state)
-                logging.info('url {} successfully loaded'.format(url))
+
+                logging.info('{} successfully loaded.'.format(url))
                 if kwargs['from_file']:
-                    file.close()
+                    _file.close()
             except Exception:
-                logging.exception('Error loading url {}. Continuing...'.format(url))
+                logging.exception('Error loading {}. Continuing...'.format(url))
                 continue
 
-        logging.info('{} providers loaded for {}'.format(len(providers), issuer_id))
+        logging.info('{} providers loaded for {}.'.format(len(providers), issuer_id))
 
-        # Load data into a DataFrame then export as csv.
+        # Load data into a DataFrame then export as CSV.
         try:
             individuals = pd.DataFrame(providers)
             print(individuals.head())
-            individuals.rename(columns={
-                'first': 'first name', 'last': 'last name', 'middle': 'middle name'}, inplace=True)
+            individuals.rename(
+                columns={'first': 'first name', 'last': 'last name', 'middle': 'middle name'},
+                inplace=True
+            )
             individuals.columns = [col.title() for col in individuals.columns]
             individuals.to_csv(output_path, index=False)
-            logging.info('Csv for {} with {} rows written to {}'.format(
+            logging.info('CSV for {} with {} rows written to {}.'.format(
                 issuer_id, individuals.shape[0], output_path))
             count_by_plan[issuer_id] = individuals.shape[0]
         except AttributeError:
-            logging.exception('Something has gone wrong with loading data into the DataFrame')
-            logging.info('Sample provider causing an error: {}'.format(providers[0]))
+            logging.exception('Something has gone wrong with loading data into the DataFrame.')
+            logging.info('Sample provider causing an error: {}.'.format(providers[0]))
 
-    logging.info('Final tally of processed providers: {}'.format(count_by_plan))
+    logging.info('Final tally of processed providers: {}.'.format(count_by_plan))
 
 
 def _get_arguments():
     """Build argument parser."""
-    parser = argparse.ArgumentParser(description='This starts a measure calculation.')
-
+    parser = argparse.ArgumentParser(description='''
+        This script starts the process of converting JSON plan data to CSVs.'
+    ''')
     parser.add_argument(
         '-s', '--state',
         help='State to extract data from.',
         required=True,
-        type=str)
-
+        type=str
+    )
     parser.add_argument(
         '-ff', '--from_file',
         help='Flag for reading from filesystem instead of url.',
         action='store_true'
     )
-
     parser.add_argument(
         '-ns', '--neighboring_states',
         help='List of neighboring states to save data for.',
         required=False,
         nargs='+',
-        type=str)
-
+        type=str
+    )
     args = parser.parse_args()
-
     return args.__dict__
 
 
--- explorer/scripts/merge_issuer_csvs.py
+++ explorer/scripts/merge_issuer_csvs.py
@@ -1,33 +1,48 @@
 """
-Manual script for merging csvs into 1 large csv per state with plan info.
+Manual script for merging csvs into one large CSV per state with plan info.
 
-TODO: Incorporate this into a script with arguments.
+FIXME: Incorporate this into a script with arguments.
 """
-import pandas as pd
+import gc
+
 import logging
+
+import pandas as pd
+
 
 logging.basicConfig(level=logging.INFO)
 
 HEALTHCARE_GOV_PATH = '/home/jovyan/work/data/healthcare_gov'
-
-state = 'MO'
+state = 'FL'
 
 # Hard coded due to lack of Company info in Machine Readable PUF.
 # TODO: Automate this dictionary creation.
-issuer_dict = {'29416': 'BestLife', '32753': 'Anthem', '74483': 'Cigna'}
+issuer_dict = {
+    '16842': 'BCBS',
+    '21663': 'Celtic/Ambetter',
+    '30252': '30252',
+    '36194': '36194',
+    '43274': '43274',
+    '48129': '48129',
+    '54172': 'Molina',
+    '56503': '56503',
+    '93299': '93299',
+    '98869': '98869',
+}
 
 csvs = [HEALTHCARE_GOV_PATH + '/{}/{}.csv'.format(state, issuer) for issuer in issuer_dict.keys()]
-logging.info('Csvs being read in: {}'.format(csvs))
+logging.info('CSVs being read in: {}'.format(csvs))
 dfs = [pd.read_csv(csv) for csv in csvs]
 
 
 for issuer_id, df in zip(issuer_dict.keys(), dfs):
     df['IssuerId'] = int(issuer_id)
     df['CompanyName'] = issuer_dict[issuer_id]
+
 logging.info('{} provider dataframes loaded in'.format(len(dfs)))
 
 plans = pd.read_csv(HEALTHCARE_GOV_PATH + '/Plan_Attributes_PUF.csv')
-
+plans = plans[plans.StateCode == state]
 # Reduce the number of columns in the plans data.
 plans = plans[[
     'BusinessYear',
@@ -59,30 +74,33 @@ plans = plans[[
     'PlanVariantMarketingName',
     'CSRVariationType'
 ]]
-
-plans = plans[plans.StateCode == state]
 # Reduce to 1 line per Standard Component Id (a.k.a plan_id in provider file).
 plans.drop_duplicates(subset=['StandardComponentId'], inplace=True)
 plans = plans[plans.DentalOnlyPlan == 'No']
-logging.info('Number of rows in plans df: {}'.format(plans.size))
+logging.info('Number of rows in plans df: {}'.format(plans.shape[0]))
 
-in_state_plan_ids = set(plans.StandardComponentId.unique())
+in_state_plan_ids = set(plans.StandardComponentId)
 
 all_the_plans = pd.concat(dfs)
-logging.info('Number of lines in joined provider dataframes: {}'.format(all_the_plans.size))
+logging.info('Lines in concatenated provider dataframes: {}'.format(all_the_plans.shape[0]))
 all_the_plans = all_the_plans[all_the_plans.Plan_Id.isin(in_state_plan_ids)]
+logging.info('Lines in concatenated provider dataframes (in-state): {}'.format(
+    all_the_plans.shape[0]))
 
-# Reduce memory consumption
+# Reduce memory consumption.
 del dfs
+gc.collect()
 
 # Join Plan and Provider dataframes.
 logging.info('Joining plan and provider dataframes...')
 merged = pd.merge(
     all_the_plans, plans, how='left', left_on='Plan_Id', right_on='StandardComponentId')
 logging.info('Joining complete!')
-logging.info('Number of lines in the final merged dataframe: {}'.format(merged.size))
+logging.info('Number of lines in the final merged dataframe: {}'.format(merged.shape[0]))
 
 del all_the_plans
+gc.collect()
 
-merged.to_csv('all_of_{}.csv'.format(state), index=False)
-logging.info('{} lines of data for {} written to csv'.format(merged.size, state))
+target_path = 'HEALTHCARE_GOV_PATH/all_of_{}.csv'.format(state)
+merged.to_csv(target_path, index=False)
+logging.info('{} lines of data for {} written to csv'.format(merged.shape[0], state))
