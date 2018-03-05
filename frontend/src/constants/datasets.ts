926437dea7e208b9050f2e605d9bd953725767f4
Eric Boucher <eric.p.boucher@gmail.com>
2018-03-05 15:31:39 -0800

    Add emptyListToFullState function and hints (#484)
    * Empty serviceAreaIds for fl endoc
    
    * Add emptyListToFullState function and hints
    
    * Remove TODO
    
    * Change name

--- frontend/src/constants/datasets.ts
+++ frontend/src/constants/datasets.ts
@@ -1,12 +1,19 @@
 import { Dataset } from './datatypes'
+import { SERVICE_AREAS_BY_STATE } from './zipCodes'
 
 export let DATASETS: Dataset[] = [
   // FIXME: Programatically read in datasets.
-  // TODO: If no service area IDs, then assume full state.
   // tslint:disable:no-var-requires
-  require('./datasets/MS_HCSD_and_Look-Alike_MS.json'),
-  require('./datasets/FL_HCSD_and_Look-Alike_FL.json'),
-  require('./datasets/FL_fl_endocrinologists.json'),
-  require('./datasets/TX_HCSD_and_Look-Alike_TX.json'),
-  require('./datasets/TX_texas_abortion_clinics_address_mar2017.json')
+  inferServiceAreaIds(require('./datasets/MS_HCSD_and_Look-Alike_MS.json')),
+  inferServiceAreaIds(require('./datasets/FL_HCSD_and_Look-Alike_FL.json')),
+  inferServiceAreaIds(require('./datasets/FL_fl_endocrinologists.json')),
+  inferServiceAreaIds(require('./datasets/TX_HCSD_and_Look-Alike_TX.json')),
+  inferServiceAreaIds(require('./datasets/TX_texas_abortion_clinics_address_mar2017.json'))
 ]
+
+function inferServiceAreaIds(dataset: Dataset) {
+  if (dataset.serviceAreaIds.length === 0) {
+    dataset.serviceAreaIds = SERVICE_AREAS_BY_STATE[dataset.state]
+  }
+  return dataset
+}
