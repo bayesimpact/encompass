d0a24d6a3f89c0fc4549befb8a9570627cf2def6
Eric Boucher <eric.p.boucher@gmail.com>
2018-03-05 15:28:04 -0800

    Change name

--- frontend/src/constants/datasets.ts
+++ frontend/src/constants/datasets.ts
@@ -4,14 +4,14 @@ import { SERVICE_AREAS_BY_STATE } from './zipCodes'
 export let DATASETS: Dataset[] = [
   // FIXME: Programatically read in datasets.
   // tslint:disable:no-var-requires
-  emptyListToFullState(require('./datasets/MS_HCSD_and_Look-Alike_MS.json')),
-  emptyListToFullState(require('./datasets/FL_HCSD_and_Look-Alike_FL.json')),
-  emptyListToFullState(require('./datasets/FL_fl_endocrinologists.json')),
-  emptyListToFullState(require('./datasets/TX_HCSD_and_Look-Alike_TX.json')),
-  emptyListToFullState(require('./datasets/TX_texas_abortion_clinics_address_mar2017.json'))
+  inferServiceAreaIds(require('./datasets/MS_HCSD_and_Look-Alike_MS.json')),
+  inferServiceAreaIds(require('./datasets/FL_HCSD_and_Look-Alike_FL.json')),
+  inferServiceAreaIds(require('./datasets/FL_fl_endocrinologists.json')),
+  inferServiceAreaIds(require('./datasets/TX_HCSD_and_Look-Alike_TX.json')),
+  inferServiceAreaIds(require('./datasets/TX_texas_abortion_clinics_address_mar2017.json'))
 ]
 
-function emptyListToFullState(dataset: Dataset) {
+function inferServiceAreaIds(dataset: Dataset) {
   if (dataset.serviceAreaIds.length === 0) {
     dataset.serviceAreaIds = SERVICE_AREAS_BY_STATE[dataset.state]
   }
