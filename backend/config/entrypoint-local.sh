2600e5f3a67873563e8b7d0d5c8b3a6c5dc87cdc
EC2 Default User <ec2-user@ip-172-31-46-70.us-west-2.compute.internal>
2018-03-06 22:53:28 +0000

    update docker-compose

--- backend/config/entrypoint-local.sh
+++ backend/config/entrypoint-local.sh
@@ -10,7 +10,7 @@ python runners/check_sample_points_exist.py
 FIRST_INIT=$?
 if (($FIRST_INIT == 1)); then
     echo "Performing load of initial Encompass data"
-    python runners/load_representative_points.py -f data/sample/los-angeles-points.geojson
+    python runners/load_representative_points.py -f data/sample/tanzania.geojson
     python runners/load_addresses.py -f data/sample/mock-providers.csv
 else
     echo "Bypassing load of initial Encompass data"
