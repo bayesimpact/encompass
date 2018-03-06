b89251ac38cd90372c31c590acf6ce1a50512187
philip <philip@bayesimpact.org>
2018-03-06 11:04:10 -0800

    Add react-ga package and setup basic pageview tracking

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react'
 import { render } from 'react-dom'
+import ReactGA from 'react-ga';
 import { App } from './components/App/App'
 
+ReactGA.initialize('UA-115165680-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 render(<App />, document.querySelector('#App'))
