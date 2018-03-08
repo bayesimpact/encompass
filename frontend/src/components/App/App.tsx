8017f8a88c9c2151ddaa6e480dc9af8961fbc5a8
philip <philip@bayesimpact.org>
2018-03-08 10:38:11 -0800

    Try to use Roboto for the whole app

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -5,6 +5,7 @@ import { withStore } from '../../services/store'
 import { ErrorBar } from '../ErrorBar/ErrorBar'
 import { SuccessBar } from '../ErrorBar/ErrorBar'
 import { FilterBar } from '../FilterBar/FilterBar'
+import { FontLink } from '../FontLink/FontLink'
 import { Header } from '../Header/Header'
 import { LeftPane } from '../LeftPane/LeftPane'
 import { MapView } from '../MapView/MapView'
@@ -13,6 +14,7 @@ import './App.css'
 export let App = withStore('error', 'success')(({ store }) =>
   <MuiThemeProvider>
     <div className='App'>
+      <FontLink />
       <Header />
       <FilterBar />
       <MapView />
---
+++ frontend/src/components/FontLink/FontLink.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react'
+
+export let FontLink: React.StatelessComponent = () =>
+  <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' />
