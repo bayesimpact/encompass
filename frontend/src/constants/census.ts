4012bedeaacca4c13c8a035bff4a8b78e98b9b37
Eric Boucher <eric.p.boucher@gmail.com>
2018-03-12 17:05:01 -0700

    Use full datasets and more counties

--- frontend/src/constants/census.ts
+++ frontend/src/constants/census.ts
@@ -10,5 +10,35 @@ type censusMapping = {
 export const CENSUS_MAPPING_ERROR = 'No Census Mapping Detected'
 
 export const CENSUS_MAPPING: censusMapping = {
-  all: ['All']
+  age: [
+    '0-18 Years',
+    '19-25 Years',
+    '26-34 Years',
+    '35-54 Years',
+    '55-64 Years',
+    '65+ Years'
+  ],
+  sex: ['Male', 'Female'],
+  race: [
+    'Hispanic or Latino (any race)',
+    'White',
+    'Black',
+    'American Indian & Alaska Native',
+    'Asian',
+    'Native Hawaiian & other Pacific Islander',
+    'Multiracial or Other'
+  ],
+  insurance: [
+    'Private Health Insurance',
+    'Public Health Insurance',
+    'No Health Insurance'
+  ],
+  income: [
+    '< $15k',
+    '$15k - $50k',
+    '$50k - $100k',
+    '$100k - $150k',
+    '$150k - $200k',
+    '> $200k'
+  ]
 }
