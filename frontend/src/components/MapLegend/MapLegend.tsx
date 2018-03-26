f1671cf736aa7f6a1729b3b8c112dc562eb91b7e
philip <philip@bayesimpact.org>
2018-03-26 14:24:38 -0700

    Adjust table/csv headers per @viviandien 's direction.

--- frontend/src/components/MapLegend/MapLegend.tsx
+++ frontend/src/components/MapLegend/MapLegend.tsx
@@ -30,18 +30,18 @@ export function getLegend(method: Method, standard: AdequacyMode) {
   switch (method) {
     case 'straight_line':
       switch (standard) {
-        case AdequacyMode.ADEQUATE_0: return '0 to 10 miles'
-        case AdequacyMode.ADEQUATE_1: return '10 to 20 miles'
-        case AdequacyMode.ADEQUATE_2: return '20 to 30 miles'
-        case AdequacyMode.INADEQUATE: return 'over 30 miles'
+        case AdequacyMode.ADEQUATE_0: return '0-10 mi'
+        case AdequacyMode.ADEQUATE_1: return '10-20 mi'
+        case AdequacyMode.ADEQUATE_2: return '20-30 mi'
+        case AdequacyMode.INADEQUATE: return '30+ mi'
       }
       break
     case 'driving_time':
       switch (standard) {
-        case AdequacyMode.ADEQUATE_0: return '0 to 30 min'
-        case AdequacyMode.ADEQUATE_1: return '30 to 45 min'
-        case AdequacyMode.ADEQUATE_2: return '45 to 60 min'
-        case AdequacyMode.INADEQUATE: return 'over 60 min'
+        case AdequacyMode.ADEQUATE_0: return '0-30 min'
+        case AdequacyMode.ADEQUATE_1: return '30-45 min'
+        case AdequacyMode.ADEQUATE_2: return '45-60 min'
+        case AdequacyMode.INADEQUATE: return '60+ min'
       }
       break
   }
