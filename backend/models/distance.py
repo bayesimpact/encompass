8f1c4e77c4803b0a0546238ca7278dc69b7a3d21
Brian Lewis <brian@bayesimpact.org>
2018-03-27 11:26:14 -0700

    Initial commit of gravitational model for adequacy

--- backend/models/distance.py
+++ backend/models/distance.py
@@ -17,3 +17,10 @@ class HaversineDistance(Measurer):
         # TODO: Determine if this check for non-null values is necessary.
         if point_a and point_b:
             return haversine(tuple(point_a), tuple(point_b))
+
+    def _get_matrix(self, source_points, destination_points):
+        """Retrieve a distance matrix."""
+        return [
+            [self.measure_between_two_points(point_a, point_b) for point_b in destination_points]
+            for point_a in source_points
+        ]
