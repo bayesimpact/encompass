ef40308a708f95b121d131bfb985227ea745f07c
Eric Boucher <eric.p.boucher@gmail.com>
2018-04-25 14:34:30 -0700

    Add datasets

--- frontend/src/constants/datasets.ts
+++ frontend/src/constants/datasets.ts
@@ -4,7 +4,8 @@ import { SERVICE_AREAS_BY_STATE } from './zipCodes'
 export let DATASETS: Dataset[] = [
   // FIXME: Programatically read in datasets.
   // tslint:disable:no-var-requires
-  inferServiceAreaIds(require('./datasets/US_mental_health.json')),
+  inferServiceAreaIds(require('./datasets/US_Behavioral_Health_Treament_Facilities_2018_04.json')),
+  // inferServiceAreaIds(require('./datasets/US_Retailer_Locator_2018_03_FNS.json')),
   inferServiceAreaIds(require('./datasets/MS_HCSD_and_Look-Alike_MS.json')),
   inferServiceAreaIds(require('./datasets/FL_HCSD_and_Look-Alike_FL.json')),
   inferServiceAreaIds(require('./datasets/FL_fl_endocrinologists.json')),
