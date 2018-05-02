9ec6bc5fee824bc6aa88ce0a6364f9dc973202d8
Philip Dickinson <philip.jerome.dickinson@gmail.com>
2018-05-01 17:05:28 -0700

    Detect WebGL. (#707)
    * Detect WebGL.
    
    * Add real dialog for users with WebGL disabled.
    
    * Update button label casing in WebGL message.

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -2,6 +2,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import * as React from 'react'
 import '../../services/effects'
 import { withStore } from '../../services/store'
+import { isWebGLEnabled } from '../../utils/webgl'
 import { AlertDialog } from '../AlertDialog/AlertDialog'
 import { ErrorBar } from '../ErrorBar/ErrorBar'
 import { SuccessBar } from '../ErrorBar/ErrorBar'
@@ -12,10 +13,18 @@ import { MapView } from '../MapView/MapView'
 import './App.css'
 
 /**
- * Typescript complains about implicit type when using import for this package.
- *
  * Check to see if the client is mobile and display a warning if they are.
  */
+
+/**
+ * Check to see if WebGL is supported and if it isn't, offer the user the opportunity to be
+ * redirected to instructions for how to enable it.
+ */
+if (!isWebGLEnabled()){
+  if (window.confirm('Unfortunately, WebGL is not enabled in your browser and you will not be able to display the map. Click "OK" to learn more, or "Cancel" to load Encompass without the map.')){
+    window.location.href = 'https://get.webgl.org/'
+  }
+}
 
 export let App = withStore('error', 'success', 'alert')(({ store }) =>
   <MuiThemeProvider>
