9f93f211db44fb5c3647a993e23b8b3f94323e77
Eric Boucher <eric.p.boucher@gmail.com>
2018-05-03 10:42:44 -0700

    Update buildCache and split createStoeLike functions

--- frontend/scripts/buildCache.ts
+++ frontend/scripts/buildCache.ts
@@ -9,7 +9,7 @@ import { seq } from 'promise-seq'
 import { buildCsvFromData, getStaticCsvUrl } from '../src/components/DownloadAnalysisLink/BuildAnalysis'
 import { CONFIG } from '../src/config/config'
 import { DATASETS } from '../src/constants/datasets'
-import { Adequacies, Method } from '../src/constants/datatypes'
+import { Adequacies, Dataset, Method, RepresentativePoint } from '../src/constants/datatypes'
 import { State, STATES } from '../src/constants/states'
 import {
   getAdequacies, getCensusData, getRepresentativePoints, getStaticAdequacyUrl, getStaticDemographicsUrl, getStaticRPUrl
@@ -26,7 +26,7 @@ const METHODS: Method[] = ['driving_time', 'straight_line']
 */
 const s3Bucket = 'encompass-public-data'
 const s3 = new S3()
-const uploadToS3 = false
+const uploadToS3 = true
 const cacheUSAWide = false
 
 main()
@@ -56,26 +56,22 @@ async function testAPIAccess() {
 function cacheData() {
   return seq(...DATASETS.map(dataset => async () => {
     let states: State[] = dataset.usaWide && cacheUSAWide ? STATES.map(_ => _.shortName as State) : [dataset.state]
+    console.log(`Dealing with ${safeDatasetHint(dataset)}...`)
     return seq(...states.map(state => async () => {
       dataset.state = state
       console.log(`  Getting Rps and Census for ${safeDatasetHint(dataset)}`)
       let representativePoints = await getRepresentativePoints({ service_area_ids: dataset.serviceAreaIds })
       let census = await getCensusData({ service_area_ids: dataset.serviceAreaIds })
-      let storeLikeRps = representativePoints.map(_ => ({
-        ..._,
-        population: Number(_.population),
-        serviceAreaId: _.service_area_id,
-        demographics: census[_.service_area_id]
-      }))
+
+      let rpParams = { Bucket: s3Bucket, Key: getS3Key(getStaticRPUrl(dataset)) }
+      let censusParams = { Bucket: s3Bucket, Key: getS3Key(getStaticDemographicsUrl(dataset)) }
+
+      await uploadIFFNotExists(rpParams, JSON.stringify(representativePoints), 'Rps ' + safeDatasetHint(dataset))
+      await uploadIFFNotExists(censusParams, JSON.stringify(census), 'Census ' + safeDatasetHint(dataset))
+
       console.log(`  Done getting Rps and Census for ${safeDatasetHint(dataset)}`)
 
-      if (uploadToS3) {
-        console.log(`  Uploading Rps and Census for ${safeDatasetHint(dataset)}`)
-        let rpParams = { Bucket: s3Bucket, Key: getS3Key(getStaticRPUrl(dataset)), Body: JSON.stringify(representativePoints), ContentType: 'application/json', ACL: 'public-read' }
-        let censusParams = { Bucket: s3Bucket, Key: getS3Key(getStaticDemographicsUrl(dataset)), Body: JSON.stringify(census), ContentType: 'application/json', ACL: 'public-read' }
-        s3.putObject(rpParams, s3Callback)
-        s3.putObject(censusParams, s3Callback)
-      }
+      let storeLikeRps = createStoreLikeRps(representativePoints, census)
 
       return seq(...METHODS.map(method => async () => {
         console.log('  Getting Adequacy and result CSV for ' + safeDatasetHint(dataset) + ' for ' + method)
@@ -86,29 +82,16 @@ function cacheData() {
           dataset_hint: safeDatasetHint(dataset)
         })
 
-        let hash = keyBy(storeLikeRps, 'id')
-        let storeLikeAdequacies: Adequacies = chain(storeLikeRps)
-          .map(_ => _.id)
-          .zipObject(adequacies)
-          .mapValues((_, key) => ({
-            adequacyMode: getAdequacyMode(
-              _, method, hash[key].serviceAreaId, dataset.serviceAreaIds
-            ),
-            id: _.id,
-            toClosestProvider: _.to_closest_provider,
-            closestProvider: dataset.providers[_.closest_providers[0]]
-          }))
-          .value()
+        let storeLikeAdequacies = createStoreLikeAdequacies(storeLikeRps, adequacies, method, dataset)
 
         let CSVResult = buildCsvFromData(
           method, dataset.serviceAreaIds, storeLikeAdequacies, storeLikeRps, true)
 
-        if (uploadToS3 && adequacies) {
-          console.log('  Uploading Adequacy and result CSV for ' + safeDatasetHint(dataset) + ' for ' + method)
-          let adequacyParams = { Bucket: s3Bucket, Key: getS3Key(getStaticAdequacyUrl(dataset, method)), Body: JSON.stringify(adequacies), ContentType: 'application/json', ACL: 'public-read' }
-          s3.putObject(adequacyParams, s3Callback)
-          let CSVResultsParams = { Bucket: s3Bucket, Key: getS3Key(getStaticCsvUrl(dataset, method)), Body: JSON.stringify(CSVResult), ContentType: 'application/json', ACL: 'public-read' }
-          s3.putObject(CSVResultsParams, s3Callback)
+        if (adequacies) {
+          let adequacyParams = { Bucket: s3Bucket, Key: getS3Key(getStaticAdequacyUrl(dataset, method)) }
+          let CSVResultsParams = { Bucket: s3Bucket, Key: getS3Key(getStaticCsvUrl(dataset, method)) }
+          await uploadIFFNotExists(adequacyParams, JSON.stringify(adequacies), 'Adequacies ' + safeDatasetHint(dataset))
+          await uploadIFFNotExists(CSVResultsParams, JSON.stringify(CSVResult), 'CSV Results ' + safeDatasetHint(dataset))
         }
         console.log('  Done getting Adequacy and result CSV for ' + safeDatasetHint(dataset) + ' for ' + method)
       }))
@@ -121,4 +104,42 @@ function getS3Key(s3URL: string) {
   return s3Key
 }
 
-function s3Callback(err: any, data: any) { console.log(JSON.stringify(err) + ' ' + JSON.stringify(data)) }
+function s3Callback(err: any, data: any) { err ? console.log(JSON.stringify(err) + ' ' + JSON.stringify(data)) : console.log('Success') }
+
+function uploadIFFNotExists(params: S3.HeadObjectRequest, body: string, hint: string, forceS3Upload?: boolean) {
+  s3.headObject(params, function (err, _) {
+    if (forceS3Upload || err && uploadToS3) {
+      console.log(`  Uploading to S3 for ${hint}`)
+      params = Object.assign(params, { Body: body, ContentType: 'application/json', ACL: 'public-read' })
+      s3.putObject(params, s3Callback)
+    } else {
+      console.log(`  Object already exists for ${hint}`)
+    }
+  })
+}
+
+function createStoreLikeRps(representativePoints: any, census: any) {
+  return representativePoints.map(_ => ({
+    ..._,
+    population: Number(_.population),
+    serviceAreaId: _.service_area_id,
+    demographics: census[_.service_area_id]
+  }))
+}
+
+function createStoreLikeAdequacies(storeLikeRps: RepresentativePoint[], adequacies: any[], method: Method, dataset: Dataset) {
+  let hash = keyBy(storeLikeRps, 'id')
+  let storeLikeAdequacies: Adequacies = chain(storeLikeRps)
+    .map(_ => _.id)
+    .zipObject(adequacies)
+    .mapValues((_, key) => ({
+      adequacyMode: getAdequacyMode(
+        _, method, hash[key].serviceAreaId, dataset.serviceAreaIds
+      ),
+      id: _.id,
+      toClosestProvider: _.to_closest_provider,
+      closestProvider: dataset.providers[_.closest_providers[0]]
+    }))
+    .value()
+  return storeLikeAdequacies
+}
