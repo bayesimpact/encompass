20dc7b8bf97e0e24d598853c29a97221f448d7f2
Andrea Egan <akegan@users.noreply.github.com>
2018-05-08 17:02:11 -0700

    Rearrange datasets (#770)

--- frontend/src/constants/datasets.ts
+++ frontend/src/constants/datasets.ts
@@ -6,11 +6,11 @@ export let DATASETS: Dataset[] = [
   // tslint:disable:no-var-requires
   inferServiceAreaIds(require('./datasets/US_mental_health.json')),
   inferServiceAreaIds(require('./datasets/US_snap_farmers_markets.json')),
+  inferServiceAreaIds(require('./datasets/TX_texas_abortion_clinics_address_mar2017.json')),
+  inferServiceAreaIds(require('./datasets/TX_HCSD_and_Look-Alike_TX.json')),
   inferServiceAreaIds(require('./datasets/MS_HCSD_and_Look-Alike_MS.json')),
   inferServiceAreaIds(require('./datasets/FL_HCSD_and_Look-Alike_FL.json')),
-  inferServiceAreaIds(require('./datasets/FL_fl_endocrinologists.json')),
-  inferServiceAreaIds(require('./datasets/TX_HCSD_and_Look-Alike_TX.json')),
-  inferServiceAreaIds(require('./datasets/TX_texas_abortion_clinics_address_mar2017.json'))
+  inferServiceAreaIds(require('./datasets/FL_fl_endocrinologists.json'))
 ]
 
 function inferServiceAreaIds(dataset: Dataset) {
